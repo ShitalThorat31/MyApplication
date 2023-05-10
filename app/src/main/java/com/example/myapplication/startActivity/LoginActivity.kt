2ecc7de1147833ce8f671b4029c36520831b87aa
ShitalThorat31 <sthorat31@gmail.com>
2023-05-10 12:59:34 +0530

    Loign Acitivity Added

---
+++ app/src/main/java/com/example/myapplication/startActivity/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.startActivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+
+
+        }
+
+
+}
